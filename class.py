cdac14f9beee4fc8e020eea7820f81fe4a87876f
HEJOHHE <prdsj17@gmail.com>
2022-10-26 11:56:20 -0300

    agora sim

---
+++ class.py
@@ -0,0 +1,7 @@
+class carro:
+    def __init__(self,marca,tempo_de_uso,qualidade_de_pneu):
+        self.marca = marca
+        self.tempo_de_uso = tempo_de_uso
+        self.qualidade_de_pneu = qualidade_de_pneu
+        
+        pass
---
+++ voltando.py
@@ -0,0 +1,11 @@
+def asterisco():
+    print(30 * '*')
+
+
+def interface():
+    asterisco()
+    print('1 - ADICIONAR CARRO\n2 - EXCLUIR CARRO\n3- INFORMAÇÕES DOS CARRO\n4 - SAIR')
+    asterisco()
+
+
+
