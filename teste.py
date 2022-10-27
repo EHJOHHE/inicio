56a5c0b21df62ac3b51980372e70b16637ef3afa
HEJOHHE <prdsj17@gmail.com>
2022-10-27 09:16:33 -0300

    kk

---
+++ teste.py
@@ -0,0 +1,26 @@
+
+# a = 1
+
+# b = 2
+
+# c = 3
+
+# lista = [(1,2),(3,4),(5,6),(7,8),(f'{a} {b} {c}')]
+# cont = 1
+# for index in lista:
+#     print(f'{cont} - {index}')
+#     cont = cont + 1
+
+lteste= []
+
+# b = ("a","b")
+# lteste.append(b)
+# print(lteste)
+# print(b)
+# b = ()
+# print(b)
+# b = (1 , 3)
+# lteste.append(b)
+# print(b)
+# print(lteste)
+
--- voltando.py
+++ voltando.py
@@ -1,3 +1,10 @@
+
+
+x = 1
+lista = []
+armazen = ()
+
+
 def asterisco():
     print(30 * '*')
 
@@ -8,4 +15,37 @@ def interface():
     asterisco()
 
 
+def func_1():
+    asterisco()
+    marca =input('DIGITE A MARCA DO SEU CARRO\n')
+    tempo_de_uso = input('QUANTOS ANOS ELE TEM\n')
+    cor = input('COR DO SEU CARRO\n')
+    armazen = (f'{marca} {tempo_de_uso} {cor} ')
+    lista.append(armazen)
+    armazen = ()
+    print('OBRIGADO PELA PARTICIPAÇÃO :)')
 
+
+def func_2():
+    c = len(lista)
+    if c == 0:
+        print("CADASTRE UM CARRO")
+    else :
+        x = int(input('DIGITE O NÚMERO DO CARRO PARA SER EXCLIDO\n'))
+        lista.pop(x-1)
+        print(lista)
+    
+
+def func_3():
+    cont = 1
+    for index in lista:
+        print(f'{cont} - {index}')
+        cont = cont + 1
+        
+while x == 1:
+    interface()
+    opção = int(input('ESCOLHA UMA OPÇÃO\n'))
+    if opção == 1:
+        func_1()
+    elif opção == 2:
+        func_2()
