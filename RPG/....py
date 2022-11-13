3ea38c25e6d947f289e445ab1247279a7be08744
HEJOHHE <prdsj17@gmail.com>
2022-11-13 00:11:00 -0300

    RPG

---
+++ RPG/....py
@@ -0,0 +1,143 @@
+import os
+
+def aste():
+    print('\033[0;31m*\033[m' * 60)
+
+
+aste2 = '*' * 26
+espa = ' '*4
+
+    
+def dialogo():
+    x = 0
+    xx = 0
+    import time
+    import os
+    print("\n" * 75)
+    print('\033[7;32mM4$¢4rA$.EXE\033[m'.center(156))
+    print('\033[1;32mTRASMISSÂO INICIADA<')
+    os.system('pause')
+    while x != 50:
+        print('TRASMISSÂO INICIADA<')
+        x = x + 1
+    time.sleep(0.10)    
+    print('O MEDO ACOMPANHA A LUA')
+    print('A LUA ACOMPANHA O MEDO')
+    time.sleep(0.10)
+    while xx != 50:
+        print('TRASMISSÂO INICIADA<')
+        xx = xx + 1
+    os.system('pause')
+    
+    print('.')
+    time.sleep(2)
+    print('.')
+    time.sleep(2)
+    print('.\033[m')
+    time.sleep(2)
+    print("\n" * 100)
+    print('\033[7;32mM4$¢4rA$.EXE\033[m'.center(156))
+    time.sleep(2)
+    print('\033[1;32m\n')
+    print('conectando com 107.26.54.0')
+    print('.')
+    time.sleep(1)
+    print('.')
+    time.sleep(1)
+    print('.')
+    time.sleep(2)
+    print('.')
+    time.sleep(4)
+    print('.')
+    
+    os.system('pause')
+    
+    time.sleep(0.50)
+    print('DEMONIO: info..')
+    time.sleep(0.50)
+    print('INFORMANTE: avestruz negra.. comfirma?')
+    time.sleep(0.50)
+    print('DEMONIO: comfirma.. continue!!')
+    time.sleep(0.50)
+    print('INFORMANTE: cinco novas uvas vindo')
+    time.sleep(0.50)
+    print('mais uma tentativa falha dos otários\033[m\033[1;31m')
+    time.sleep(0.50)
+    print('|',aste2,'|',aste2,'|')
+    print(f'|{espa}DANIEL:                 |{espa}LUIS:                   |')
+    time.sleep(0.50)
+    print(f'|{espa}INVESTIGADOR ESQUENTADO |{espa}INVESTIGADOR VERRISSMO  |\n|{espa}FOTOGRAFO               |{espa}FRACO                   |')
+    time.sleep(0.50)
+    print(f'|{espa}ELEMENTO: MORTE         |{espa}ELEMENTO: MORTE/CONHE.. |')
+    time.sleep(0.50)
+    print(f"|{espa}CONTATO COM O           |{espa}CONTATO COM O           |\n|    PARANORMAL: 20          |    PARANORMAL: 20          |")
+    time.sleep(0.50)
+    print('|',aste2,'|',aste2,'|')
+    time.sleep(4)
+    print('|',aste2,'|',aste2,'|')
+    print(f'|{espa}NIX   :                 |{espa}VICTORIO:               |')
+    time.sleep(0.50)
+    print(f'|{espa}OCULTISTA   DOENTE      |{espa}INVESTIGADOR BONITO     |\n|{espa}MEDICO DA ITALIA        |{espa}CABEÇA QUEBRADA         |')
+    time.sleep(0.50)
+    print(f'|{espa}ELEMENTO: ENERGIA       |{espa}ELEMENTO: CONHECIMENTO  |')
+    time.sleep(0.50)
+    print(f"|{espa}CONTATO COM O           |{espa}CONTATO COM O           |\n|    PARANORMAL: 20          |    PARANORMAL: 20          |")
+    time.sleep(0.50)
+    print('|',aste2,'|',aste2,'|')
+    time.sleep(4)
+    print('|',aste2,'|')
+    time.sleep(0.50)
+    print('| VITORIO : |')
+    print('ERRO')
+    time.sleep(10)
+    
+    os.system('pause')
+    
+    print('\n' * 100)
+    time.sleep(0.50)
+    print('RECONECTANDO ...')
+    time.sleep(3)
+    print('ERRO 404' * 50)
+    time.sleep(4)
+    print('IF0RM4Nt3: G#@$D0')
+    time.sleep(4)
+    print('ERRO 404' * 50)
+    time.sleep(3)
+    print('.')
+    time.sleep(3)
+    print('.')
+    time.sleep(3)
+    print('.')
+    time.sleep(4)
+    print('LOGIN PERDIDO')
+    
+    os.system('pause')
+
+
+def login():
+    x = 0
+    import time
+    print('\033[1;32mBEM-VINDO :):)')
+    print('USER: DEMONIO')
+    
+    while x != 3:
+        s = input('SENHA:\033[m\n\033[1;31m')
+        if s == "LUA.ESTA.COMIGO":
+            print('ACESSO COM SUCESSO')
+            time.sleep(1)
+            print('.')
+            time.sleep(1)
+            print('.')
+            time.sleep(1)
+            print('.')
+            time.sleep(1)
+            print('.')
+            time.sleep(10)
+            print('FALHA')
+        else :
+            x = x + 1
+            print(f'ERRADO TENS MAIS {3 - x} TENTATIVAS')
+
+
+dialogo()
+login()
---
+++ RPG/DADOS.py
@@ -0,0 +1,12 @@
+import os
+
+espa = " " * 4
+aste2 = '*' * 26 
+print('|',aste2,'|')
+print(f'|{espa}DANIEL:                 |')
+print(f'|{espa}INVESTIGADOR,ESQUENTADO |\n|{espa}FOTOGRAFO               |')
+print(f'|{espa}ELEMENTO: MORTE         |')
+print(f"|{espa}CONTATO COM O           |\n|    PARANORMAL: 20          |")
+print("\033[1;32msalve")
+os.system("pause")
+print("otaro\033[m")
